857fab9a7d25024327c40e56921607622daf07c9
dennyclain123 <dennyclain123@gmail.com>
2021-07-14 16:12:32 +0630

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Weather-App-Responsive
+NPM Modules to install:
+npm init -y
+//For Front-end
+npm i bootstrap sass 
+//For Backend
+npm i Express
+npm i node-fetch
+npm i ejs
---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+
+// Middleware
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }))
+
+// Import Route
+const weatherRoute = require('./routes/weather');
+
+// Use View Engine
+app.set('view engine', 'ejs');
+
+// Middleware route
+app.use('/', weatherRoute);
+// app.use(express.static(__dirname + "/public"));
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log(`Server starting at port ${PORT}`));
